b1df1309eefb8a98f16f1047aba009e3ffb6c061
LookRain <brantscube@hotmail.com>
2017-02-27 15:42:27 +0800

    add css and js using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,10 @@ elixir(mix => {
     mix.sass('app.scss')
        .styles('jquery.wizard.css')
        .styles('lightbox.min.css')
+       .styles('dataTables.bootstrap.min.css')
        .webpack('app.js')
        .webpack('jquery.wizard.js')
-       .webpack('lightbox.min.js');
+       .webpack('lightbox.min.js')
+       .scripts('dataTables.bootstrap.min.js')
+       .scripts('jquery.dataTables.min.js');
 });
